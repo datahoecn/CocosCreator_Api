7bf4b3c127226b6cf1c1f102b4dbe69380ec64d7
lijie00004 <lijie00004@163.com>
2019-05-21 18:02:31 +0800

    q

--- UI/ProgressBar.js
+++ UI/ProgressBar.js
@@ -4,3 +4,8 @@ ProgressBar//进度条
 	TotalLength：当进度条为横向类型时，用来设置进度达到最大值时精灵的长度。
 	Progress：表示当前默认的进度值，这里的 0.3 表示 30%。
 	Reverse：用来设置进度条沿 X 轴正向还是 X 轴反向变化，取消选中，表示沿 X 轴正向。
+
+	this.verticalBar.progress = 0;
+
+	圆型
+		BarSprite 的 type 要设为 FILLED RADIAL
--- UI/Slider.js
+++ UI/Slider.js
@@ -1,8 +1,9 @@
 Slide//滑动条
-	Handle：指定对应的滑块按钮。
+	Handle：指定 button
 	Direction：设置滑动条的方向。
 	Progress：设置当前滑块所处的进度位置。
 	Slide Events：设定滑块按钮在调节滑动条进度时的响应函数
+
 	onSliderVEvent (sender, eventType) {//分别代表了滑动条节点和事件类型,事件类型的值为 CONTROL_EVENTTYPE_VALUE_CHANGED 值
 	    this._updateImageOpacity(sender.progress);
 	},
