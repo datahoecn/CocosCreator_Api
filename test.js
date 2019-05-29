21d6a924c82cff4342eea872d5954c9c71684ff2
lijie00004 <lijie00004@163.com>
2019-05-29 18:04:54 +0800

    q

--- test.js
+++ test.js
@@ -1,6 +1,38 @@
-var  array  =  [1,  2,  3,  4];
-var newArray = array.map(function(item, i, array) {
-	console.log(item, i, array, this);
-	return item + this.a;
-}, {a: 1})
-console.log(newArray);
+(function(){
+	var getLogin = function() {
+		var a = parseInt(Math.random() * 10).toFixed(0);
+		if (a % 2 == 0) {
+			return  { login: false}
+		}
+
+		return {
+			login: true,
+			userinfo: {
+				nickname: "jask",
+				vip: 11,
+				userid: "666666"
+			}
+		}
+	}
+
+	var withLogin = function(basicFn) {
+		var loginInfo = getLogin();
+		return basicFn.bind(null, loginInfo);
+	}
+	window.withLogin = withLogin;
+})();
+
+(function() {
+	var withLogin = window.withLogin;
+	var randerIndex = function(loginInfo) {
+		if (loginInfo.login) {
+
+		} else {
+
+		}
+	}
+
+	window.renderIndex = withLogin(renderIndex)
+})();
+
+
