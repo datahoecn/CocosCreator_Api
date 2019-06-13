ec37c50b3cf5206f503d13fc60873c849f404089
lijie00004 <lijie00004@163.com>
2019-06-13 18:02:52 +0800

    q

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-var a = new Boolean(0);
-a = true;
-a = false;
-a = "111";
-b = a.valueOf();
-console.log(typeof b)
+function foo() {
+	var a = 20;
+	function bar() {
+		a = 30;
+		console.log(a);
+
+	}
+	bar();
+}
+foo()
