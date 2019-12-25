595b33fec470123da60449fa01c439190477be88
lijie00004 <lijie00004@163.com>
2019-12-25 18:03:58 +0800

    q

---
+++ SoftwareTutorial/vscode.js
@@ -0,0 +1,14 @@
+// 快捷键
+Shift + Alt + F		Format document
+F2					Go Definition
+Alt + F2			Peek Definition
+F2					Rename Symbol
+Ctrl + K Ctrl + X	Trim trailing whitespace
+Ctrl + \ 			Split editor
+Ctrl + Shift + S 	Save As... // 另存为
+Ctrl + Tab			Open next
+Ctrl + K P 			Copy path of active file
+Ctrl + K R 			Reveal active file in Explorer
+Ctrl + Shift + E    Show Explorer / Toggle focus
+Ctrl + Shift + F    Show Search
+Ctrl + Shift + H    Replace in files
