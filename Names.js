f63bc504b78f6c5531c6e1b089234d8c8adca276
lijie00004 <lijie00004@163.com>
2020-01-09 22:34:18 +0800

    q

---
+++ Names.js
@@ -0,0 +1,137 @@
+calculate		calc	计算
+multiplication	mul	乘法
+division		div	除法
+hexadecimal		hex	十六进制
+
+list			lst	列表
+Sequence		seq	
+Segment(s)		seg	
+stack			stk	栈
+dictionary		dict	字典
+character		char	字符
+text			txt	文本
+float			flt	浮动、浮点
+image			img	图像
+bitmap			bmp	位图
+table			tbl	表
+link			lnk	链接
+lable			lbl	标签
+flag			flg	标志
+container		cntr	容器
+time stamp		ts	时间戳
+length			len	长度
+positive		pos			正数
+negative		neg 	负数
+
+
+statistic		stat	统计
+summation		sum	和
+average			avg	平均
+middle			mid	中值
+increase		inc	增加
+decrease		dec	减少
+different		diff	不同的
+frequency		freq	频率
+optimization	opt	最优
+total			tot	全部的
+vertical		vert	垂直
+horizontal		horz	水平
+
+
+row				row	行
+column			col	列
+point			pt	点
+pointer			ptr	指针
+index			idx / ndx	索引、指示
+value			val	值
+reference		ref	引用
+
+status			stat	状态
+original		orig	原件
+source			src	源头
+address			addr	地址
+coordinates		coord	坐标
+previous		pre	前一个
+destination		dest	目的
+iteration		iter	循环、迭代
+count			cnt	计数器
+
+
+temporary		temp	临时
+resource		res	资源
+result			res	结果
+return			ret	返回
+answer			ans	响应、回答
+buffer			buf	缓冲区
+
+database		db	数据库
+administrator	adm	管理员
+password		pwd	密码
+user			usr	用户
+
+directory		dir	目录
+document		doc	文档
+library			lib	库
+function		func	函数
+object			obj	对象
+argument		arg	实参
+instance		ins	实例
+variable		var	变量
+parameter		param	参数(形参)
+
+
+encode			enc	编码
+print			prn	打印
+delete			del	删除
+insert			ins	插入
+error			err	错误
+break			brk	间断
+package			pkg	打包
+escape			esc	退出
+execute			exec	执行
+command			cmd	命令
+configuration	config	配置
+edit			edt	编辑
+display			disp	显示
+initialize		init	初始化
+trigger			trig	触发
+capture			cap或capt	捕获
+system			sys	系统
+environment		env	环境
+window			win(wnd)	窗口
+device			dev	设备
+message			msg	消息
+signal			sig	信号
+information		info	信息
+
+
+getInstance    //获得实例
+upData        //数据更新
+onRender    //渲染    
+init...        //初始化...
+set...        //设置/更改 
+loadFile    //装载文件
+add..by..    //添加...
+moveDown    //移动
+backup        //备份
+revert        //恢复
+londFile    //读取文件
+Birth        //初始
+Vaule        //值
+index        //索引
+create...    //创建        
+Entity      //（父类用）
+Monster        //怪物
+Scene        //场景
+Node        //节点
+director    //主管
+stance        //立场
+select        //挑选
+refresh        //更新、使.....恢复
+Global         //全局、全球
+Register    //注册
+Controller     //控制器
+
+类的数据成员加前缀m_（表示member），这样可以避免数据成员与成员函数的参数同名 m_width 
+常量全用大写的字母，用下划线分割单词。 MAX_LENGTH 
+类名和函数名用大写字母开头的单词组合而成
